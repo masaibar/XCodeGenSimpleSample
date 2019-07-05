bd4c7aac17f7dc55fee282f814a9e8144782cc94
masaibar <masaibar.dev@gmail.com>
2019-07-05 15:27:06 +0900

    first commit

---
+++ XCodeGenSimpleSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XCodeGenSimpleSample
+//
+//  Created by Nishihara Ryo on 2019/07/05.
+//  Copyright © 2019 Nishihara Ryo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
